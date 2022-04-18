486eb238c2ca21307c6a4811c6441dda474ad12e
Русак Дмитрий Сергеевич <rusak@rit.va>
2022-04-18 18:16:45 +0300

    [PROCESSING] Add Get

--- CacheServer.go
+++ CacheServer.go
@@ -5,6 +5,7 @@ import (
 	redisRepository "app/internal/repository/redis"
 	"context"
 	"fmt"
+	"github.com/fatih/structs"
 	"github.com/go-redis/redis/v8"
 	"github.com/rs/xid"
 	log "github.com/sirupsen/logrus"
@@ -17,11 +18,13 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	config.InitLogger()
 	ctx := context.Background()
 	repoCashEntity := redisRepository.NewCashSteamEntityRep(configuration.REDIS_URL)
-	consumersGroup := "tickets-consumer-group"
-	err = repoCashEntity.Client().XGroupCreateMkStream(ctx, repoCashEntity.StreamName, consumersGroup, "0").Err()
+	consumersGroup := "CacheService"
+	repoCashEntity.Client().XGroupCreate(ctx, repoCashEntity.StreamGet, consumersGroup, "0")
+	repoCashEntity.Client().XGroupCreate(ctx, repoCashEntity.StreamSet, consumersGroup, "0")
 	if err != nil {
 		log.Println(err)
 	}
@@ -31,8 +34,8 @@ func main() {
 		entries, err := repoCashEntity.Client().XReadGroup(ctx, &redis.XReadGroupArgs{
 			Group:    consumersGroup,
 			Consumer: uniqueID,
-			Streams:  []string{repoCashEntity.StreamName, ">"},
-			Count:    2,
+			Streams:  []string{repoCashEntity.StreamSet, repoCashEntity.StreamGet, ">", ">"},
+			Count:    0,
 			Block:    0,
 			NoAck:    false,
 		}).Result()
@@ -40,27 +43,64 @@ func main() {
 			log.Fatal(err)
 		}
 
-		for i := 0; i < len(entries[0].Messages); i++ {
-			values := entries[0].Messages[i].Values
-			key := fmt.Sprintf("%v", values["key"])
-			deathTime, _ := strconv.Atoi(values["DeathTime"].(string))
+		for _, query := range entries {
+			switch query.Stream {
+			case repoCashEntity.StreamGet:
+				for i := 0; i < len(entries[0].Messages); i++ {
+					values := entries[0].Messages[i].Values
+					idEntity := fmt.Sprintf("%v", values["idEntities"])
+					idStream := fmt.Sprintf("%v", values["idOutput"])
 
-			if int64(deathTime) < time.Now().Unix() {
-				log.WithError(err).Error("Time end")
-				break
-			}
+					entity, err := repoCashEntity.CashEntityRepositoryRedis.Get(ctx, idEntity)
+					if err != nil {
+						log.WithError(err).Error("sGet in redis")
+						err = repoCashEntity.Client().XAdd(ctx, &redis.XAddArgs{
+							Stream: idStream,
+							Values: map[string]interface{}{"status": 404},
+						}).Err()
+					}
 
-			cacheObj := redisRepository.CacheObj{
-				DeathTime: int64(deathTime),
-				Data:      []byte(values["Data"].(string)),
-			}
+					mapData := structs.Map(entity)
 
-			err = repoCashEntity.Client().Set(ctx, key, cacheObj, 0).Err()
-			if err != nil {
-				log.WithError(err).Error("Error Set")
+					err = repoCashEntity.Client().XAdd(ctx, &redis.XAddArgs{
+						Stream: idStream,
+						Values: mapData,
+					}).Err()
+					if err != nil {
+						fmt.Println(idStream)
+						log.WithError(err).Error("sGet in redis")
+						break
+					}
+
+					repoCashEntity.Client().XAck(ctx, repoCashEntity.StreamGet, consumersGroup, entries[0].Messages[i].ID)
+					repoCashEntity.Client().XDel(ctx, repoCashEntity.StreamGet, entries[0].Messages[i].ID)
+
+					fmt.Println(entity)
+				}
+
+			case repoCashEntity.StreamSet:
+				for i := 0; i < len(entries[0].Messages); i++ {
+					values := entries[0].Messages[i].Values
+					key := fmt.Sprintf("%v", values["key"])
+					deathTime, _ := strconv.Atoi(values["DeathTime"].(string))
+
+					if int64(deathTime) < time.Now().Unix() {
+						log.WithError(err).Error("Time end")
+						break
+					}
+
+					cacheObj := redisRepository.CacheObj{
+						DeathTime: int64(deathTime),
+						Data:      []byte(values["Data"].(string)),
+					}
+
+					err = repoCashEntity.Client().Set(ctx, key, cacheObj, 0).Err()
+					if err != nil {
+						log.WithError(err).Error("Error Set")
+					}
+					log.Info("Successful Set in cache")
+				}
 			}
-			log.Info("Successful Set in cache")
 		}
 	}
-
 }
--- internal/repository/redis/chashEntityStream.go
+++ internal/repository/redis/chashEntityStream.go
@@ -4,20 +4,28 @@ import (
 	"app/internal/models"
 	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"github.com/fatih/structs"
 	"github.com/go-redis/redis/v8"
+	"github.com/rs/xid"
 	"github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
 )
 
 type CashSteamEntityRep struct {
-	StreamName string
+	StreamSet string
+	StreamGet string
+	GroupName string
 	CashEntityRepositoryRedis
 }
 
 func NewCashSteamEntityRep(addr string) *CashSteamEntityRep {
 	return &CashSteamEntityRep{
-		StreamName:                "entitySet",
+		StreamGet:                 "entityGet",
+		StreamSet:                 "entitySet",
+		GroupName:                 "Reader",
 		CashEntityRepositoryRedis: *NewCashEntityRepository(addr),
 	}
 }
@@ -34,7 +42,7 @@ func (rr CashSteamEntityRep) Set(c context.Context, entity *models.Entity) error
 	data["key"] = key
 
 	arg := redis.XAddArgs{
-		Stream: rr.StreamName,
+		Stream: rr.StreamSet,
 		MaxLen: 0,
 		ID:     "",
 		Values: data,
@@ -46,4 +54,57 @@ func (rr CashSteamEntityRep) Set(c context.Context, entity *models.Entity) error
 		return err
 	}
 	return nil
+}
+
+func (rr CashSteamEntityRep) Get(c context.Context, id string) (models.Entity, error) {
+	var cacheObj CacheObj
+	ent := models.Entity{}
+	logrus.Info("Try get with cache")
+
+	idOutStream := xid.New().String()
+
+	err := rr.client.XAdd(c, &redis.XAddArgs{
+		Stream: rr.StreamGet,
+		Values: map[string]interface{}{"idEntities": id, "idOutput": idOutStream},
+	}).Err()
+
+	if err != nil {
+		logrus.Error(err)
+		return ent, err
+	}
+
+	fmt.Println(rr.client.XGroupCreateMkStream(c, idOutStream, rr.GroupName, "0"))
+
+	newContext, cancelFunc := context.WithTimeout(c, time.Millisecond*3)
+	defer cancelFunc()
+
+	value, err := rr.client.XReadGroup(newContext, &redis.XReadGroupArgs{
+		Group:   rr.GroupName,
+		Streams: []string{idOutStream, ">"},
+		Count:   1,
+	}).Result()
+	if err != nil {
+		logrus.Error(err)
+		return ent, err
+	}
+
+	fmt.Println(value)
+	//TODO Unpars from map[string]interface to model.Entity
+	err = json.Unmarshal([]byte("das"), &cacheObj)
+	if err != nil {
+		return ent, err
+	}
+
+	if cacheObj.DeathTime < time.Now().Unix() {
+		_ = rr.Delete(c, id)
+		logrus.WithError(err).Info("Get in redis")
+		return ent, errors.New("time expired")
+	}
+
+	err = json.Unmarshal(cacheObj.Data, &ent)
+	if err != nil {
+		return ent, err
+	}
+	logrus.Info("From Cache")
+	return ent, nil
 }
