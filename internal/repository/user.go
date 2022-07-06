816fc14583c5f42e944be19dfd1b72f166671e56
Dzmitry Rusack <rusack.dmitr.work@gmail.com>
2022-07-06 18:03:47 +0300

    [ADD] CRUD table user postgres

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -3,12 +3,13 @@ package repository
 import (
 	"app/internal/models"
 	"context"
+	"os/user"
 )
 
 type RepoUser interface {
-	Get(ctx context.Context, username string) (models.User, error)
+	Get(ctx context.Context, username string) (*models.User, error)
 	Add(ctx context.Context, user models.User) error
 	Delete(ctx context.Context, username string) error
 	GetAll(ctx context.Context) (*[]models.User, error)
-	Update(ctx context.Context) error
+	Update(ctx context.Context, user *user.User) error
 }
